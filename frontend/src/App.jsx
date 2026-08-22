482efddb3f57995e5f871497bd7d0a689d1d3c1d
Durgesh Kumar <dkprajapat702@gmail.com>
2026-08-22 21:58:29 +0530

    Connect frontend to Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/products")
+    fetch("https://dk-textile-backend.onrender.com/products")
       .then((response) => response.json())
       .then((data) => {
         setProducts(data);
